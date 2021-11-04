6bf0bea1759a0be82fffa9b9be7c43f30dcf49a3
davi <davisoarescoura@gmail.com>
2021-11-05 00:18:43 +0100

    Adiciona requisito 2 ao projeto

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,33 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from '../components/About';
+
+describe('testando requisito 2, teste about', () => {
+  test('Testa se existe um heading h2 com o texto About Pokedex', () => {
+    render(<About />);
+
+    const aboutPokedex = screen.getByRole('heading', {
+      level: 2,
+      name: 'About Pokédex',
+    });
+
+    expect(aboutPokedex).toBeInTheDocument();
+  });
+
+  test('Testa se contem 2 paragrafos com o texto About Pokedex', () => {
+    render(<About />);
+
+    const firstParargaph = screen.getByText(/this/i);
+    expect(firstParargaph).toBeInTheDocument();
+
+    const secondParagraph = screen.getByText(/one/i);
+    expect(secondParagraph).toBeInTheDocument();
+  });
+
+  test('Testa se contem a imagem especifica citada de uma pokedex', () => {
+    render(<About />);
+    const image = screen.getByRole('img');
+    const { src } = image;
+    expect(src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
